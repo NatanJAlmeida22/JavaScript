e46859444f10f5b0d1f0c9f3111cb3cb15a80bb3
Natan José de Almeida <natanalmeida158@hotmail.com>
2021-10-11 14:46:11 -0300

    Create ambiente.js

---
+++ curso_em_video/aula15/ambiente.js
@@ -0,0 +1,10 @@
+let num = [5, 8, 2, 9, 3];
+// Vai colocar em ordem crescente
+num.sort()
+// Coloca o elemento sempre na última posição
+num.push(1)
+console.log(num);
+// Saber o comprimento do Array | Lenght é um atributo
+console.log(`O vetor tem ${num.length} posições`);
+// Mostrar o vetor
+console.log(`O primeiro valor é ${num[0]}`);
